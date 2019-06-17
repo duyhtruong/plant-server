6cfd051612f86479caeaacc506651c8cfe794cc4
Duy Truong <duytruongh@gmail.com>
2019-06-17 12:48:42 -0700

    logout

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,7 +3,8 @@ const User = require('../models/user')
 
 const auth = async (req,res,next) =>{
     try{
-        const token = req.header('Authorization').replace('Bearer ', '')
+
+        const token = req.header('Authorization')
         const decoded = jwt.verify(token, 'secretplaceholder')
         const user = await User.findOne({ _id: decoded._id, 'tokens.token': token})
 
